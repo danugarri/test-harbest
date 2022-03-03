594e287a16d5989d432e7b355d2b09e6cd1fb134
daniel <danielgarridolizana@gmail.com>
2022-03-03 17:54:54 +0100

    basic table

--- src/components/ProductsList/ProductsList.tsx
+++ src/components/ProductsList/ProductsList.tsx
@@ -5,9 +5,20 @@ import { getProducts } from '../service/productsList.service';
 export const ProductsList = () => {
     interface Iproducts {
         name: string;
-
+        description: string;
+        active: boolean;
+        price: number;
+        SKU: string;
     }
-    const [products,setProducts] = useState([{name:''}]);
+    const initialState= {
+        name:'daniel',
+        description: 'front',
+        active: true,
+        price: 100000,
+        SKU:'D000000'
+    }
+    const tableHeaders= Object.keys(initialState);
+    const [products,setProducts] = useState([initialState]);
     getProducts().then(response =>{ 
        const resp:Iproducts[]= response.data;
         setProducts(resp);
@@ -15,19 +26,29 @@ export const ProductsList = () => {
 
 
     return (
-        <>
+        <table>
+            <thead>
+                <tr>
+                {
+                    tableHeaders.map( (head,index) => (
+                    <th key={index}>
+                        {head}
+                    </th>
+                    ))
+                }
+                </tr>
+            </thead>
+            <tbody>
+                 <tr>
             {
-            products.map(product => (
-                 <table key= 'products'>
-                    <tbody>
-                        <tr>
-                            {product.name}
-                        </tr>
-                    </tbody>
-                </table>
-            ) )
-           
+            products.map((product,index) => {
+                
+               return Object.values(product).map(cell => <td key= {index}>{cell} </td>)
+               
+            })
             }
-        </>
+            </tr>
+            </tbody>
+        </table>
     )
 }
