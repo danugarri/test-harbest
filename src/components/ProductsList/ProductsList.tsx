0342b5e3c653db06c000239480760156c3bfe904
daniel <danielgarridolizana@gmail.com>
2022-03-05 09:41:39 +0100

    rendering all products on a table

--- src/components/ProductsList/ProductsList.tsx
+++ src/components/ProductsList/ProductsList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 import { getProducts } from '../service/productsList.service';
 
@@ -19,11 +19,13 @@ export const ProductsList = () => {
     }
     const tableHeaders= Object.keys(initialState);
     const [products,setProducts] = useState([initialState]);
-    getProducts().then(response =>{ 
-       const resp:Iproducts[]= response.data;
-        setProducts(resp);
-    })
-
+   
+    useEffect(() => {
+        getProducts().then(response =>{ 
+        const resp:Iproducts[]= response.data;
+            setProducts(resp);
+        })
+    },[])
 
     return (
         <table>
@@ -38,16 +40,23 @@ export const ProductsList = () => {
                 }
                 </tr>
             </thead>
-            <tbody>
-                 <tr>
+            <tbody> 
             {
-            products.map((product,index) => {
-                
-               return Object.values(product).map(cell => <td key= {index}>{cell} </td>)
-               
-            })
+                products.map((product,index) => {
+                    const row= Object.values(product);
+
+                    return (
+                    <tr>
+                        {
+                            row.map(cell => (
+                                cell === true || cell === false ? <td key= {index}>{`${cell}`} </td> :
+                                <td key= {index}>{cell} </td>
+                            ))
+                        }
+                        </tr>
+                )
+                })
             }
-            </tr>
             </tbody>
         </table>
     )
--- src/components/service/productsList.service.tsx
+++ src/components/service/productsList.service.tsx
@@ -1,10 +1,10 @@
 import axios from "axios"
 
-const url = 'http://localhost:9000/products'
+const endPoint = 'http://localhost:9000/products'
 
 export const getProducts = () => {
-
-    
+    const url= `${endPoint}`
+    // axios.defaults.headers.common['page'] = 1;
     const response = axios.get(url);
     return response;
 }
